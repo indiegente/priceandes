941a7aeae3a291793dde7bad8aa2118d13577b47
indiegente <jorgebastidasureta@gmail.com>
2015-02-03 00:42:05 -0500

    Init Price Andes Business Module Created by @cloudbitpe Generated by [yo angular generator]

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name priceandesApp
+ * @description
+ * # priceandesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('priceandesApp', [
+    'ngAnimate',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name priceandesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the priceandesApp
+ */
+angular.module('priceandesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
